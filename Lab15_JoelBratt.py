5b4afbf0df030a8d26fbd3981dbca696089a9bb5
Joel Bratt <jbratt1@student.cscc.edu>
2026-07-20 15:05:58 -0400

    made the loop for making square

--- Lab15_JoelBratt.py
+++ Lab15_JoelBratt.py
@@ -8,3 +8,11 @@ import math
 
 x_list = []
 y_list = []
+
+for i in range(5):
+    x = math.pow(-1, math.floor((i + 1) / 2))
+    y = math.pow(-1, math.floor(i / 2))
+
+    x_list.append(x)
+    y_list.append(y)
+
